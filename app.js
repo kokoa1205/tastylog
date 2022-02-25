49923f7f56063bd3ee7119fee5a5093e0fd15105
ruku <ritorito0715@gmail.com>
2022-02-26 00:38:15 +0900

    log4jsの設定

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const PORT = process.env.PORT;
 const path = require("path");
+const logger = require("./lib/log/logger.js");
 const express = require("express");
 const favicon = require("serve-favicon");
 const app = express();
@@ -17,5 +18,5 @@ app.use("/", require("./routes/index.js"));
 
 // Excute web application
 app.listen(PORT, () =>{
-  console.log(`Application listening at ${PORT}`);
+  logger.console.info(`Application listening at ${PORT}`);
 });
